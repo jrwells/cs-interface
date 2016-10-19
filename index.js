f1b9d92fa5c7eb554e11f47579279e7528b8acf0
Robert Wells <rwells@modelab.is>
2016-10-19 13:07:06 -0400

    change to export null if not inside adobe application

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 if ((window && window.__adobe_cep__) || (global.window && global.window.__adobe_cep__)) {
   module.exports = require('./lib/CSInterface.js');
 } else {
-  module.exports = false;
+  module.exports = null;
 }
